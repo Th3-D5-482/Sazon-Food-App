9372639da15195949378f132b611b35f1e3d89e3
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-04-03 12:48:47 +0530

    Th3_D5_482

--- Codex01/app/src/main/java/com/example/codex01/Helpers/ManagmentCart.java
+++ Codex01/app/src/main/java/com/example/codex01/Helpers/ManagmentCart.java
@@ -30,26 +30,6 @@ public class ManagmentCart {
         this.tinyDB=new TinyDB(context);
     }
 
-    /*public void insertFood(Foods item) {
-        ArrayList<Foods> listpop = getListCart();
-        boolean existAlready = false;
-        int n = 0;
-        for (int i = 0; i < listpop.size(); i++) {
-            if (listpop.get(i).getTitle().equals(item.getTitle())) {
-                existAlready = true;
-                n = i;
-                break;
-            }
-        }
-        if(existAlready){
-            listpop.get(n).setNumberInCart(item.getNumberInCart());
-        }else{
-            listpop.add(item);
-        }
-        tinyDB.putListObject("CartList",listpop);
-        Toast.makeText(context, "Added to Cart", Toast.LENGTH_SHORT).show();
-    }*/
-
     public void insertFood(Foods item) {
         SharedPreferences sharedPreferences = context.getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
         String userEmail = sharedPreferences.getString("email", "");
@@ -99,16 +79,6 @@ public class ManagmentCart {
         }
         return fee;
     }
-
-    /*public void minusNumberItem(ArrayList<Foods> listItem,int position,ChangeNumberItemsListener changeNumberItemsListener){
-        if(listItem.get(position).getNumberInCart()==1){
-            listItem.remove(position);
-        }else{
-            listItem.get(position).setNumberInCart(listItem.get(position).getNumberInCart()-1);
-        }
-        tinyDB.putListObject("CartList",listItem);
-        changeNumberItemsListener.change();
-    }*/
 
     public void minusNumberItem(ArrayList<Foods> listItem, int position, ChangeNumberItemsListener changeNumberItemsListener) {
         int currentNumberInCart = listItem.get(position).getNumberInCart();
@@ -196,12 +166,6 @@ public class ManagmentCart {
                     }
                 });
     }
-
-    /*public  void plusNumberItem(ArrayList<Foods> listItem,int position,ChangeNumberItemsListener changeNumberItemsListener){
-        listItem.get(position).setNumberInCart(listItem.get(position).getNumberInCart()+1);
-        tinyDB.putListObject("CartList",listItem);
-        changeNumberItemsListener.change();
-    }*/
 
     public void plusNumberItem(ArrayList<Foods> listItem, int position, ChangeNumberItemsListener changeNumberItemsListener) {
         SharedPreferences sharedPreferences = context.getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
