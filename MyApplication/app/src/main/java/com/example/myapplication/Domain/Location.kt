b1741de00ba40cba9a01f21a288ea5094d8e0020
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-07-01 00:10:32 +0530

    Th3_D5_482

--- MyApplication/app/src/main/java/com/example/myapplication/Domain/Location.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.Domain
-
-class Location {
-    var Id = 0
-    var Loc: String? = null
-    override fun toString(): String {
-        return Loc!!
-    }
-}
--- MyApplication/app/src/main/java/com/example/myapplication/Domain/Time.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.Domain
-
-class Time {
-    var Id = 0
-    var Value: String? = null
-    override fun toString(): String {
-        return Value!!
-    }
-}
